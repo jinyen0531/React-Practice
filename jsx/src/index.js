439f27c6f8142ffa01f00538fdc14847f11a4019
jinyen0531 <34853361+jinyen0531@users.noreply.github.com>
2019-04-18 15:51:44 +0800

    JSX const replace value , class -> className, for -> htmlFor

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,22 +2,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function getButtonText() {
-  return "Click on me!";
-}
-
 // Create a react component
 const App = () => {
-  const buttonText = "Click Me!";
+  const buttonText = { text: "Click me" };
+  const labelText = "Enter Name :";
 
   return (
     <div>
-      <label className="label" for="name">
-        Enter name:
+      <label className="label" htmlFor="name">
+        {labelText}
       </label>
       <input id="name" type="text" />
       <button style={{ backgroundColor: "blue", color: "white" }}>
-        {getButtonText()}
+        {buttonText.text}
       </button>
     </div>
   );
