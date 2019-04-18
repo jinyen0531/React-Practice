96cb396fb3e6cafdd55098cb48bf004da4bb6827
jinyen0531 <34853361+jinyen0531@users.noreply.github.com>
2019-04-18 17:48:10 +0800

    use props.param to show value to different component

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Jin" />
-      <CommentDetail author="Brian" />
-      <CommentDetail author="Bruno" />
+      <CommentDetail author="Jin" timeAgo="Today at 4:00PM" />
+      <CommentDetail author="Brian" timeAgo="Today at 2:30PM" />
+      <CommentDetail author="Bruno" timeAgo="Yesterday at 5:10PM" />
     </div>
   );
 };
