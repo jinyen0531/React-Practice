68329ecb8ec5e9af9adaf7d51563429ab2b48787
jinyen0531 <34853361+jinyen0531@users.noreply.github.com>
2019-04-18 17:53:56 +0800

    replace src faker.image to props.avatar . use param to show value

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Jin" timeAgo="Today at 4:00PM" />
-      <CommentDetail author="Brian" timeAgo="Today at 2:30PM" />
-      <CommentDetail author="Bruno" timeAgo="Yesterday at 5:10PM" />
+      <CommentDetail
+        author="Jin"
+        timeAgo="Today at 4:00PM"
+        content="Nice blog post!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Brian"
+        timeAgo="Today at 2:30PM"
+        content="Just want to sleep!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Bruno"
+        timeAgo="Yesterday at 5:10PM"
+        content="Workoff Fast!!!"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
