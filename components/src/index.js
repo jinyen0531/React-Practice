a31a8f553b802f94575272b49593faf409cd0fa5
jinyen0531 <34853361+jinyen0531@users.noreply.github.com>
2019-04-19 13:57:34 +0800

    put ApprovalCard outside the CommentDetail, use props.children to show content inside card

--- components/src/index.js
+++ components/src/index.js
@@ -7,25 +7,30 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Jin"
-        timeAgo="Today at 4:00PM"
-        content="Nice blog post!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Brian"
-        timeAgo="Today at 2:30PM"
-        content="Just want to sleep!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Bruno"
-        timeAgo="Yesterday at 5:10PM"
-        content="Workoff Fast!!!"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Jin"
+          timeAgo="Today at 4:00PM"
+          content="Nice blog post!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Brian"
+          timeAgo="Today at 2:30PM"
+          content="Just want to sleep!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Bruno"
+          timeAgo="Yesterday at 5:10PM"
+          content="Workoff Fast!!!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
